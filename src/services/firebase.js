0ab50ec3c972f972fbac9109bcfabff9c7d48006
indakuldeep19-ops <indakuldeep19@gmail.com>
2026-05-25 10:02:44 +0530

    Add Firebase Functions to firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
 
+// Replace with your own Firebase config
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
@@ -14,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const functions = getFunctions(app);

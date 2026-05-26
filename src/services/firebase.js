35b35357ea658d1e5ebd3e08ce58401bdaa20d3e
indakuldeep19-ops <indakuldeep19@gmail.com>
2026-05-26 11:55:07 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
 
-// Replace with your own Firebase config
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
@@ -16,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app);

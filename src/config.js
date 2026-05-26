61c1615d1145ffca928741967e7b783b1604d554
indakuldeep19-ops <indakuldeep19@gmail.com>
2026-05-26 09:18:38 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbyf9s_ohX8YmlAnu1POmVcRHJ4IHLPzebxen-QHM8fPrFTQO8wW8G5Bhnodx3pq_aZtig/exec/exec';

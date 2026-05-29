a0564b38e6891188d0f0c3fece8d14a07fb2ecb7
indakuldeep19-ops <indakuldeep19@gmail.com>
2026-05-29 08:36:50 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// Replace with your Google Apps Script Web App URL
-export const API_BASE_URL = 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbyf9s_ohX8YmlAnu1POmVcRHJ4IHLPzebxen-QHM8fPrFTQO8wW8G5Bhnodx3pq_aZtig/exec/exec';
+export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzZaebzAxL2d8DM-OqVhn9r9_gYdETgrIa2QCBPX6uAejzpwaCDSzomdw5WzUwMnaP2wQ/exec';
